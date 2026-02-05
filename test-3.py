1729957afae45b892a33373741caba29459f7a56
tossy0130 <34308569+tossy0130@users.noreply.github.com>
2026-02-05 11:23:23 +0900

    ADD_26_0205_001

--- test-3.py
+++ test-3.py
@@ -15,23 +15,64 @@ if "collection" not in st.session_state:
     )
 
 ### Ollama モデルを使ったベクトル化
+def ollama_embed(text):
+    r = requests.post(
+        "http://localhost:12000/api/embeddings",
+        json={"model": "nomic-embed-text", "prompt": text}
+    )
+    data = r.json()
+    return data["embedding"] # ベクトル化して返す
 
 ### Wordファイルを読み込む
+def load_word_document(file):
+    return "¥n" . join(para.text for para in Document(file).paragraphs) # 行ごとの、データを改行で結合
 
 ### テキスト分割
-
+def split_text(text):
+    chunk_size = 200
+    overlap = 50
+    chunks = []
+    start = 0
+    while start < len(text):
+        end = start + chunk_size
+        chunks.append(text[start:end])
+        start += chunk_size - overlap
+    return chunks
 
 # ページタイトル
 st.set_page_config(page_title="ローカル LLM Chat")
 
-st.sidebar.title("Tossy ローカル LLM")
+# サイドバー
+st.sidebar.title("Tossy ローカル LLM RAG")
 st.sidebar.title("設定")
+
 model = st.sidebar.text_input("モデル名", value="llama3.1:8b")
 temperature = st.sidebar.slider("temperature", 0.0, 2.0, 0.3, 0.1)
 system_prompt = st.sidebar.text_area(
     "System Prompt",
     "あなたの有能なアシスタントです。日本語で回答してください。"
 )
+
+# ファイルのアップロード
+uploaded_files = st.sidebar.file_uploader(
+    "wordファイルをアップロード（.docx）",
+    type=["docx"],
+    accept_multiple_files=True # 複数ファイルアップロード OK
+)
+
+### アップロードされたファイルを、テキスト分割して、ベクトル化して、chroma_db　へ保存
+if st.sidebar.button("インデックス作成"): # ボタンを押した時のイベント
+    for file in uploaded_files:
+        text = load_word_document(file) # テキスト化
+        chunks = split_text(text) # テキスト分割
+        for i, chunk in enumerate(chunks):
+            embed = ollama_embed(chunk) # ベクトル化
+            st.session_state.collection.add(
+                documents=[chunk],
+                embeddings=[embed],
+                ids=[f"{file.name}_{i}"]
+            )
+    st.sidebar.success("インデックス作成処理 正常終了")
 
 # 会話の履歴を保管
 if "messages" not in st.session_state:
@@ -69,6 +110,37 @@ if prompt:
     #　ユーザ側のメッセージ表示
     with st.chat_message("user"):
         st.write(prompt)
+        
+    ### RAG検索 ###
+    # ベクトル化
+    query_embed = ollama_embed(prompt) 
+    
+    # 似ているベクトルを引っ張ってくる, chroma_db 
+    results = st.session_state.collection.query(
+        query_embeddings=[query_embed],
+        n_results=2 # 取得情報
+    )
+    
+    ### 返ってくるデータ構造
+    #{
+    #    "ids":
+    #    "documents": [["doc1", "doc2"]],
+    #    "distances": [[ ***, ****]] # 距離が返ってくる
+    #}
+    
+    if results["documents"]:
+       context_text = "\n".join(results["documents"][0])
+       rag_prompt = f"""
+       以下は関連ドキュメントの抜粋です。
+       {context_text}
+       この情報を参考に以下の質問に答えてください。
+       {prompt}
+       """
+       final_user_prompt = rag_prompt
+    else:
+       final_user_prompt = prompt 
+       
+    st.session_state.messages.append({"role": "user", "content": final_user_prompt})
         
     # システムプロンプト処理
     if system_prompt.strip():
